e8b22355b7a3ce8a5ef251a1a3af6e94d701c71e
Vishwas Modi <vishwasmodi220@gmail.com>
2021-12-31 01:55:17 +0530

    To deploy

--- backend/index.js
+++ backend/index.js
@@ -41,4 +41,12 @@ app.use("/api/messages", messages);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log("listening to port 5000"));
